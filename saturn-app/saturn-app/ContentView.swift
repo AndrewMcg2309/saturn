7b1a8e2e70aa954bcaccea1148719d536f7d546f
Andrew McGowan <andrew.mcgowan@immedis.com>
2024-04-21 15:50:23 +0100

    hide sensitive data

--- saturn-app/saturn-app/ContentView.swift
+++ saturn-app/saturn-app/ContentView.swift
@@ -24,36 +24,6 @@ struct ContentView: View {
     }
 }
 
-func AwsStart(){
-    let url_start = URL(string: "https://fyrbzl2e5j.execute-api.eu-west-1.amazonaws.com/startWebsite")!
-    let message = Message(
-        message: "Request from iPhone"
-    )
-    struct Message: Encodable {
-        let message: String
-    }
-    let data = try! JSONEncoder().encode(message)
-    
-    var request = URLRequest(url: url_start)
-    request.httpMethod = "POST"
-    request.httpBody = data
-    request.setValue(
-        "application/json",
-        forHTTPHeaderField: "Content-Type"
-    )
-    
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        let statusCode = (response as! HTTPURLResponse).statusCode
-
-        if statusCode == 200 {
-            print("Succesful POST")
-        } else {
-            print("POST failed")
-        }
-    }
-    task.resume()
-}
-
 #Preview {
     ContentView()
 }
